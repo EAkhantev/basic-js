f9c06e6ca9f49bef6b73b4b6f9e7c38d15449943
e.akhantev <e.akhantev@s7.ru>
2023-12-25 23:59:46 +0400

    feat: 3 tasks left to solve

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  calculateDepth(arr) {
+    const totalArrayDimention = arr.reduce((acc, item) => {
+      const itemIsArray = Array.isArray(item);
+  
+      if (itemIsArray) {
+        const itemDimention = this.calculateDepth(item);
+
+        if (itemDimention > acc) {
+          acc = itemDimention;
+        }
+      }
+      
+      return acc;
+    }, 0);
+
+    return totalArrayDimention + 1;
   }
 }
 
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+'--discard-next' // remove next elment
+'--discard-prev' // remove preview elem
+'--double-next' // dublicate next elm
+'--double-prev' // dublicate preview elm 
+function transform(arr) {
+  const argIsArray = Array.isArray(arr);
+  if (!argIsArray) {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
+  let skip = false;
+
+  return arr.reduce((acc, item, index) => {
+    const previousItem = arr[index - 1];
+    const nextItem = arr[index + 1];
+
+    if (item === '--discard-next') {
+      if (index === arr.length) return acc; 
+      skip = true;
+      return acc;
+    }
+    if (item === '--discard-prev') {
+      if (!index) return acc;
+      if (previousItem != acc[acc.length - 1]) return acc;
+      acc.pop();
+      return acc;
+    }
+    if (item === '--double-next') {
+      if (index === arr.length) return acc; 
+      if (nextItem) acc.push(nextItem);
+      return acc;
+    }
+    if (item === '--double-prev') {
+      if (!index) return acc;
+      if (previousItem != acc[acc.length - 1]) return acc;
+      acc.push(previousItem);
+      return acc;
+    }
+
+    if (skip) {
+      skip = false;
+      return acc;
+    }
+    
+    
+    acc.push(item);
+    return acc;
+  }, []);
 }
 
 module.exports = {
